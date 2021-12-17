96936931824ab477f4b4e02cc7b66710ef4b3fea
SaeedKabootari <saeedkabootari@gmail.com>
2021-12-17 12:01:48 -0800

    design index page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const threedotsButton = document.querySelector('.threedots');
+const chevronleftButton = document.querySelector('.chevronleft');
+const followButton = document.querySelector('#followbutton');
+const messageButton = document.querySelector('#messagebutton');
+const angledownButton = document.querySelector('#angledownbutton');
+
+threedotsButton.addEventListener('click' , () => console.log('threedotsButton clicked'));
+chevronleftButton.addEventListener('click' , () => console.log('chevronleftButton clicked'));
+followButton.addEventListener('click' , () => console.log('followButton clicked'));
+messageButton.addEventListener('click' , () => console.log('messageButton clicked'));
+angledownButton.addEventListener('click' , () => console.log('angledownButton clicked'));
